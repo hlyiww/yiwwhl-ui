af5b4fc77b0a6e9c65349a412398a507e977b0ea
yiwwhl <yiwwhl@qq.com>
2022-10-23 19:45:36 +0800

    feat: Button 组件 MVP 实现

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
 <script setup lang="ts"></script>
-
+<template>
+  <YButton>按钮</YButton>
+</template>
 <style scoped></style>
--- src/button/src/Button.tsx
+++ src/button/src/Button.tsx
@@ -1,4 +1,11 @@
-const Button = () => (
-  <div class="text-3xl font-bold underline">Button 按钮</div>
-);
-export default Button;
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "YButton",
+  setup(props, { slots }) {
+    return () => {
+      const defaultSlot = slots.default ? slots.default() : "按钮";
+      return <button>{defaultSlot}</button>;
+    };
+  },
+});
