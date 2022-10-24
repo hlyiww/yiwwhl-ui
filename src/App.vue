b1d8d15f0d7ce5044b88ae8994f8c21bf2c2f186
yiwwhl <yiwwhl@qq.com>
2022-10-24 20:46:11 +0800

    refactor(Button): refactor button style

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { Button } from "../src/button/index";
 </script>
 <template>
   <Button type="primary">确定</Button>
+  <Button type="text">文本确定</Button>
   <Button>取消</Button>
 </template>
 <style scoped></style>
