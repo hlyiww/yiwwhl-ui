4cf05db0db867f754ee85aaac498b3647de79f03
yiwwhl <yiwwhl@qq.com>
2022-10-24 21:20:42 +0800

    feat(Button): button disabled

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
 import { Button } from "../src/button/index";
 </script>
 <template>
-  <Button type="primary" size="large">确定</Button>
-  <Button type="primary" size="medium">确定</Button>
-  <Button type="primary" size="small">确定</Button>
-  <Button type="text">文本确定</Button>
-  <Button size="small">取消</Button>
+  <Button type="primary" size="large" disabled>确定</Button>
+  <Button type="text" disabled>文本确定</Button>
+  <Button size="small" disabled>取消</Button>
 </template>
 <style scoped></style>
